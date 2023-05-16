b351c8d8f1feb21376604ba373d0a6fa09f208f0
Netology QA <obgynkostina@gmail.com>
2023-05-16 23:44:57 +0300

    first tests

--- src/test/java/ChangeDateTest.java
+++ src/test/java/ChangeDateTest.java
@@ -3,12 +3,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 import java.util.Locale;
 
-
+import static com.codeborne.selenide.Selenide.open;
 
 public class ChangeDateTest {
+
+    @BeforeEach
+    void setUp() { open ("http://localhost:9999/");}
     public static Faker faker;
 
     @BeforeAll
@@ -16,14 +18,14 @@ public class ChangeDateTest {
         faker = new Faker(new Locale("ru"));
     }
     @Test
+    
     void ShouldGenerateTestData(){
         String name = faker.name().fullName();
         String phone = faker.phoneNumber().phoneNumber();
+
         printTestData(name,phone);
     }
 
     private void printTestData(String name, String phone) {
     }
-
-
 }
