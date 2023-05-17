fdca8eb6fc35a8c7c607b7f6b311ac633f346e68
Netology QA <obgynkostina@gmail.com>
2023-05-17 15:47:05 +0300

    test is ready

--- src/test/java/ChangeDateTest.java
+++ src/test/java/ChangeDateTest.java
@@ -1,31 +1,55 @@
+import com.codeborne.selenide.Condition;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
+
+import java.time.Duration;
 import java.util.Locale;
 
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class ChangeDateTest {
 
     @BeforeEach
-    void setUp() { open ("http://localhost:9999/");}
-    public static Faker faker;
-
-    @BeforeAll
-    static void setFaker() {
-        faker = new Faker(new Locale("ru"));
+    void setUp() {
+        open("http://localhost:9999/");
     }
+
     @Test
-    
-    void ShouldGenerateTestData(){
-        String name = faker.name().fullName();
-        String phone = faker.phoneNumber().phoneNumber();
+    void ShouldChangeMeetingDate() {
+        DataGenerator.Registration.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
+        int daysForFirstMeeting = 4;
+        String firstMeetingDate = DataGenerator.generateDate(daysForFirstMeeting);
+        int daysForSecondMeeting = 7;
+        String secondMeetingDate = DataGenerator.generateDate(daysForSecondMeeting);
+        $("[data-test-id = city] input").setValue(validUser.getCity());
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id = date]  input").setValue(firstMeetingDate);
+        $("[data-test-id = name] input").setValue(DataGenerator.generateName("ru")) ;
+        $("[data-test-id = phone] input").setValue(DataGenerator.generatePhoneNumber("ru"));
+        $("[data-test-id = agreement]").click();
+        $(".button__text").shouldHave(Condition.text("Запланировать")).click();
+        $(".notification__title").shouldHave(Condition.text("Успешно!")).shouldBe(Condition.visible,
+                Duration.ofSeconds(15));
+        $(".notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " +
+                firstMeetingDate)).shouldBe(Condition.visible);
 
-        printTestData(name,phone);
-    }
 
-    private void printTestData(String name, String phone) {
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id = date]  input").setValue(secondMeetingDate);
+        $(".button__text").shouldHave(Condition.text("Запланировать")).click();
+        $("[data-test-id= replan-notification]").shouldHave
+                (Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?")).shouldBe(Condition.visible,Duration.ofSeconds(15));
+        $("[data-test-id= replan-notification] button").click();
+        $(".notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " +
+                secondMeetingDate)).shouldBe(Condition.visible);
+
+
     }
 }
+
+
+
